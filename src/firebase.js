53bcbf923b4edc1550aeeaf0d70202142b598a4f
Yash Chavan <yashchavan4628@gmail.com>
2024-08-19 09:38:09 +0530

    final Commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,6 @@ import 'firebase/compat/auth';
 import 'firebase/compat/storage';
 import 'firebase/compat/database';
 
-console.log('API Key:', process.env.REACT_APP_FIREBASE_API_KEY);
-console.log('Auth Domain:', process.env.REACT_APP_AUTH_DOMAIN);
-// Add more console logs for other variables if necessary
-
 const app = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -16,6 +12,16 @@ const app = firebase.initializeApp({
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 });
+
+// const app = firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID
+// });
 
 export const auth = app.auth();
 export const storage = app.storage();
